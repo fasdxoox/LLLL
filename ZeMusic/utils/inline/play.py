a4ff1b1f3132238778e824c0206394ed82ba9879
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-04-10 17:35:53 +0300

    Update play.py

--- ZeMusic/utils/inline/play.py
+++ ZeMusic/utils/inline/play.py
@@ -67,7 +67,7 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
          ],[
-            InlineKeyboardButton(text="『 𝙺𝙸𝙽𝙶 𝚂𝚄𝙾𝚁𝙲𝙴 』", url=f"https://t.me/EF_19")],
+            InlineKeyboardButton(text="𝐬𝐨𝐮𝐫𝐜𝐞 𝐬𝐢𝐦𝐚", url=f"https://t.me/yut70")],
     ]
     return buttons
 
@@ -81,7 +81,7 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
          ],[
-            InlineKeyboardButton(text="『 𝙺𝙸𝙽𝙶 𝚂𝚄𝙾𝚁𝙲𝙴 』", url=f"https://t.me/EF_19")],
+            InlineKeyboardButton(text="𝐬𝐨𝐮𝐫𝐜𝐞 𝐬𝐢𝐦𝐚", url=f"https://t.me/yut70")],
     ]
     return buttons
 
