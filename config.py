fa9ad25c5838628d2ea2b9ac0ee2b1740c6e6d1e
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-04-10 19:59:14 +0300

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/king-1-9/AMusic",
+    "https://github.com/king-1-9/SMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
