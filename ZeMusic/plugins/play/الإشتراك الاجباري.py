d8b835326e4fc47548b64886902946918cf685b9
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-04-10 18:16:31 +0300

    Add files via upload

---
+++ ZeMusic/plugins/play/الإشتراك الاجباري.py
@@ -0,0 +1,34 @@
+"""
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+from ZeMusic import app
+
+Muntazer ="EF_19"
+@app.on_message(filters.incoming & filters.private, group=-1)
+async def must_join_channel(app: Client, msg: Message):
+    if not Muntazer:
+        return
+    try:
+        try:
+            await app.get_chat_member(Muntazer, msg.from_user.id)
+        except UserNotParticipant:
+            if Muntazer.isalpha():
+                link = "https://t.me/" + Muntazer
+            else:
+                chat_info = await app.get_chat(Muntazer)
+                link = chat_info.invite_link
+            try:
+                await msg.reply(
+                    f"~︙عزيزي {msg.from_user.mention} \n~︙عليك الأشتراك في قناة البوت \n~︙قناة البوت : @{Muntazer}.",
+                    disable_web_page_preview=True,
+                    reply_markup=InlineKeyboardMarkup([
+                        [InlineKeyboardButton("< 𝙺𝙸𝙽𝙶 >", url=link)]
+                    ])
+                )
+                await msg.stop_propagation()
+            except ChatWriteForbidden:
+                pass
+    except ChatAdminRequired:
+        print(f"I m not admin in the MUST_JOIN chat {Muntazer}!")
+"""
---
+++ ZeMusic/plugins/play/قناة الاشتراك.py
@@ -0,0 +1,18 @@
+"""
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from ZeMusic import app
+from config import OWNER_ID
+
+Muntazer = "EF_19"
+@app.on_message(filters.private & filters.user(OWNER_ID))
+async def must_join_channel(_, message):
+    if "‹ قناة الاشتراك ›" in message.text:
+        link = f"https://t.me/{Muntazer}"
+        await message.reply(
+            text=f"~ عزيزي المطور \n~ هذا هي قناة الاشتراك الاجباري @{Muntazer} .",
+            reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton("~ 𝙺𝙸𝙽𝙶 .", url=link)]
+            ])
+        )
+        """
