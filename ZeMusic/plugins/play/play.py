14f1855446635894732f788cb5529a38527edb37
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-04-21 18:09:06 +0300

    Update play.py

--- ZeMusic/plugins/play/play.py
+++ ZeMusic/plugins/play/play.py
@@ -32,7 +32,7 @@ from config import BANNED_USERS, lyrical
             "تشغيل",
             "شغل",
             "vplay",
-            "فديو",
+            "vتشغيل",
             "cplay",
             "cvplay",
             "playforce",
