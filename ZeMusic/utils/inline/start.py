415db3a3e21f85da6775a8cd2bd96b01ea9755a1
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-04-25 12:22:33 +0300

    Update start.py

--- ZeMusic/utils/inline/start.py
+++ ZeMusic/utils/inline/start.py
@@ -8,14 +8,14 @@ def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="⌯ أضفني إلى مجموعتك ⌯",
+                text="أضفني إلى مجموعتك",
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
-        [InlineKeyboardButton(text="⌯ الأوامر ⌯", callback_data="zzzback")],
+        [InlineKeyboardButton(text="الأوامر", callback_data="zzzback")],
         [
-            InlineKeyboardButton(text="⌯ المطور ⌯", user_id=config.OWNER_ID),
-            InlineKeyboardButton(text="⌯ 𝐬𝐨𝐮𝐫𝐜𝐞 𝐬𝐢𝐦𝐚 ⌯", url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text="المطور", user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=config.CHANNEL_NAME, url=config.CHANNEL_LINK),
         ],
     ]
     return buttons
@@ -25,14 +25,14 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="⌯ أضفني إلى مجموعتك ⌯",
+                text="أضفني إلى مجموعتك",
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
-        [InlineKeyboardButton(text="⌯ الأوامر ⌯", callback_data="zzzback")],
+        [InlineKeyboardButton(text="الأوامر", callback_data="zzzback")],
         [
-            InlineKeyboardButton(text="⌯ المطور ⌯", user_id=config.OWNER_ID),
-            InlineKeyboardButton(text="⌯ 𝐬𝐨𝐮𝐫𝐜𝐞 𝐬𝐢𝐦𝐚 ⌯", url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text="المطور", user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=config.CHANNEL_NAME, url=config.CHANNEL_LINK),
         ],
     ]
     return buttons
