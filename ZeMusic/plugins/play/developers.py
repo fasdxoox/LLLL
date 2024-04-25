33ccec3d2efe52bb411ce3ef26d937b4c2eb656e
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-04-25 12:26:25 +0300

    Update developers.py

--- ZeMusic/plugins/play/developers.py
+++ ZeMusic/plugins/play/developers.py
@@ -16,10 +16,10 @@ from random import  choice, randint
 #          
                 
 @app.on_message(
-  command(["المبرمج","مبرمج السورس","مبرمج"])
+  command(["المبرمج","مبرمج السورس","مبرمج","مطور السورس"])
 )
 async def huhh(client: Client, message: Message):
-    dev_id = 5145609515
+    dev_id = 6304465500
     dev = await client.get_users(dev_id)
     name = dev.first_name
     usrnam = dev.username
@@ -28,7 +28,7 @@ async def huhh(client: Client, message: Message):
    
     await message.reply_photo(
         photo="downloads/developer.jpg",
-        caption=f"""<b>⌯ 𝙽𝙰𝙼𝙴 :</b> <a href='https://t.me/{usrnam}'>{name}</a>\n\n<b>⌯ 𝚄𝚂𝙴𝚁𝙽𝙰𝙼𝙴 :</b> {usrnam}""",
+        caption=f"""<b>⌯ 𝙽𝙰𝙼𝙴 :</b> <a href='https://t.me/{usrnam}'>{name}</a>\n\n<b>⌯ 𝚄𝚂𝙴𝚁𝙽𝙰𝙼𝙴 :</b> @{usrnam}""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
