56d7263dfba4d6045cd3a34891f18d7b99df8a3f
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-05-04 11:02:26 +0300

    Update play.py

--- ZeMusic/utils/inline/play.py
+++ ZeMusic/utils/inline/play.py
@@ -61,12 +61,13 @@ def stream_markup_timer(_, chat_id, played, dur):
                 callback_data="GetTimer",
             )
         ],[
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-         ],[
+            InlineKeyboardButton(text="إستئناف ⌯", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="إيقاف مؤقت", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="⌯ إعاده", callback_data=f"ADMIN Replay|{chat_id}"),
+        ],[
+            InlineKeyboardButton(text="⌯ تخطي ⌯", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="⌯ توقف ⌯", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],[
             InlineKeyboardButton(text=config.CHANNEL_NAME, url=config.CHANNEL_LINK)],
     ]
     return buttons
@@ -75,12 +76,13 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-         ],[
+            InlineKeyboardButton(text="إستئناف ⌯", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="إيقاف مؤقت", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="⌯ إعاده", callback_data=f"ADMIN Replay|{chat_id}"),
+        ],[
+            InlineKeyboardButton(text="⌯ تخطي ⌯", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="⌯ توقف ⌯", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],[
             InlineKeyboardButton(text=config.CHANNEL_NAME, url=config.CHANNEL_LINK)],
     ]
     return buttons
