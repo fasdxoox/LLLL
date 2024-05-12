11f5dec62286139990edc8d79e4fd8ee1045151a
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-05-12 23:05:37 +0300

    Update السورس.py

--- ZeMusic/plugins/play/السورس.py
+++ ZeMusic/plugins/play/السورس.py
@@ -11,7 +11,8 @@ from strings.filters import command
 from ZeMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 from ZeMusic import app
 from random import  choice, randint
-
+nem = config.CHANNEL_NAME
+che = config.CHANNEL_LINKl
                 
 @app.on_message(
     command(["سورس","السورس"])
@@ -19,12 +20,12 @@ from random import  choice, randint
 async def huhh(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://telegra.ph/file/730915b75a10e6baf5cef.jpg",
-        caption = f"""<b>⌯ 𝚆𝙴𝙻𝙲𝙾𝙼𝙴 𝚃𝙾 . .<b>\n<a href="https://t.me/yut70">⌯ 𝐬𝐨𝐮𝐫𝐜𝐞 𝐬𝐢𝐦𝐚 ⛧</a></b>""",
+        caption = f"""<b>⌯ 𝚆𝙴𝙻𝙲𝙾𝙼𝙴 𝚃𝙾 . .<b>\n<a href='{che}'>⌯ {nem}</a></b>""",
 reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        text=config.CHANNEL_NAME, url=config.CHANNEL_LINK),         
+                        text=nem, url=che),         
                 ],
 
             ]
