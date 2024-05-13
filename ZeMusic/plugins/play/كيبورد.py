4aef0878d774d6cca9493936f958c2533e571620
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-05-14 02:03:52 +0300

    Update كيبورد.py

--- ZeMusic/plugins/play/كيبورد.py
+++ ZeMusic/plugins/play/كيبورد.py
@@ -93,7 +93,7 @@ async def down(client, message):
             #[
                 #[
                     #InlineKeyboardButton(
-                        #"قناة السورس", url=f"https://t.me/EF_19"),
+                        #"قناة السورس", url=f"https://t.me/SOURCELARIN"),
                 #],[
                     #InlineKeyboardButton(
                         #"• ضيفني لقروبك 🎻", url=f"https://t.me/{app.username}?startgroup=true"),
