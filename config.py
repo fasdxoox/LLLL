0d5f58c0869c34bd514bca67a37716e3f7771ae3
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-05-14 00:20:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/king-1-9/SMusic",
+    "https://github.com/fasdxoox/LLLL",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
