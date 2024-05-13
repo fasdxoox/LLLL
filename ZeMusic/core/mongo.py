b97e09bdbf3048e1b1f600f694ede59982661b4f
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-05-14 02:20:21 +0300

    Update mongo.py

--- ZeMusic/core/mongo.py
+++ ZeMusic/core/mongo.py
@@ -4,11 +4,11 @@ from config import MONGO_DB_URI
 
 from ..logging import LOGGER
 
-LOGGER(__name__).info("يتم رفع البيانات...")
+LOGGER(__name__).info("يتم رفع البيانات على سورس لارين...")
 try:
     _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
     mongodb = _mongo_async_.Elhyba
-    LOGGER(__name__).info("تم رفع البيانات.")
+    LOGGER(__name__).info("تم رفع البيانات على سورس لارين.")
 except:
     LOGGER(__name__).error("خطاء.  لم يتم رفع البيانات")
     exit()
